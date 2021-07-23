9a6001ed16a62d9805798b7ad12b7195420081ea
Hairon Chaviano <hchaviano921011@gmail.com>
2021-07-23 16:09:44 -0300

    refactor home screen

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:firebasestarter/constants/colors.dart';
 import 'package:firebasestarter/employees/employees.dart';
+import 'package:firebasestarter/home/cubit/home_cubit.dart';
 import 'package:firebasestarter/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebasestarter/user_profile/user_profile.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
 
   static Route route() {
@@ -14,31 +16,28 @@ class HomeScreen extends StatefulWidget {
   }
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return BlocProvider(create: (_) => HomeCubit(), child: const HomePages());
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  int _index;
-  List<Widget> _screens;
-
-  @override
-  void initState() {
-    _index = 0;
-    _screens = [
-      const EmployeesScreen(),
-      const ProfileScreen(),
-    ];
-    super.initState();
-  }
+class HomePages extends StatelessWidget {
+  const HomePages({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _index = context.watch<HomeCubit>().state.pageIndex;
+    final _screens = [
+      const EmployeesScreen(),
+      const ProfileScreen(),
+    ];
     return Scaffold(
       body: _screens[_index],
       backgroundColor: AppColor.lightGrey,
       bottomNavigationBar: StarterBottomNavigationBar(
         index: _index,
-        updateIndex: (int index) => setState(() => _index = index),
+        updateIndex: (int index) =>
+            context.read<HomeCubit>().updatePageIndex(index),
       ),
     );
   }
